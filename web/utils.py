65a2d195c0c33e66cc6714d63c09289462cb9efa
teach-genius <chantryolanda85@gmail.com>
2026-05-03 15:12:41 +0100

    version-prédéploy

--- web/utils.py
+++ web/utils.py
@@ -1,53 +1,27 @@
 from dotenv import load_dotenv
-from bytez import Bytez
-from langchain.agents import create_agent
+from langchain_mistralai import ChatMistralAI
 from langchain_core.messages import HumanMessage
 import re
-import os    
+import os
 
 load_dotenv()
 
-API_KEY = os.getenv("BYTEZ_API_KEY")
-sdk = Bytez(API_KEY)
-model = sdk.model("Qwen/Qwen3-4B-Instruct-2507")
-
+llm = ChatMistralAI(
+    model="mistral-small-latest",
+    api_key=os.getenv("MISTRAL_API_KEY"),
+    temperature=0.7
+)
 
 SERVICE_UNAVAILABLE_MESSAGE = (
     "Notre assistant intelligent est temporairement indisponible. "
     "Merci de nous contacter — nous reviendrons vers vous rapidement."
 )
 
-def chatAryadResponse(messages: str) -> str:
+def chatAryadResponse(messages: list) -> str:
     try:
-        results = model.run(messages)
-        cleaned = re.sub(r'<think>.*?</think>\s*', '',results.output['content'], flags=re.DOTALL)
-        try:
-            return cleaned
-        except (AttributeError, IndexError, TypeError):
-            return SERVICE_UNAVAILABLE_MESSAGE
-
+        response = llm.invoke(messages)
+        cleaned = re.sub(r'<tool_call>.*?</tool_call>\s*', '', response.content, flags=re.DOTALL)
+        return cleaned
     except Exception as e:
-        print(f'error :{e}')
+        print(f'error : {e}')
         return SERVICE_UNAVAILABLE_MESSAGE
-
-#creation reactAgent
-def createReactAgent(llm,prompt_system:str,liste_tools: list=[]):
-    return create_agent(
-    model=llm,
-    tools=liste_tools,
-    system_prompt=prompt_system)
-
-#Inférence ReactAgent
-def call_ReactAgent(agent,message):
-    result = agent.invoke({
-    "messages": [
-        HumanMessage(
-            content=message
-            )
-        ]
-    })
-    last_message = result["messages"][-1].content
-    if isinstance(last_message, list):
-        return last_message[0]["text"]
-    else:
-        return last_message
